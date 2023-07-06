760283e85771f26d565bf30e22af32efcbc27ee7
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2023-07-06 18:16:18 -0400

    feat: create prisma connection

---
+++ prisma/migrations/20230706215009_create_tables/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Invoice" (
+    "id" TEXT NOT NULL,
+    "balance" DECIMAL(8,2) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "methodPayment" TEXT NOT NULL,
+    "datePayment" TIMESTAMP(3) NOT NULL,
+    "typePayment" TEXT NOT NULL,
+
+    CONSTRAINT "Invoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
